a76c5693cf4adf74c1e91af12e7a8ba6491f4af8
lals-m3 <16458080+lalondesteve@users.noreply.github.com>
2025-01-20 11:14:18 -0500

    langmap mappings

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,23 +1,21 @@
-
 local map = vim.keymap.set
 
 -- lazy
 map("n", "<leader>ll", "<cmd>Lazy<cr>", { desc = "Lazy" })
 map("n", "<leader>lx", "<cmd>LazyExtras<cr>", { desc = "LazyExtras" })
-
+-- utils
 map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
 map("n", "<leader>e", vim.cmd.Ex, { desc = "File Explorer" })
 -- move lines
-map({ "n", "v", "x" }, "<C-j>", ":m'>+<cr>gv")
-map({ "n", "v", "x" }, "<C-k>", ":m-2<cr>gv")
+map({ "n", "v", "x" }, "<C-j>", ":m'>+<cr>gv", { desc = "move line/selection down" })
+map({ "n", "v", "x" }, "<C-k>", ":m-2<cr>gv", { desc = "move line/selection down" })
 -- move and zoom
 map("n", "<C-d>", "<C-d>zz", { desc = "Page Down" })
 map("n", "<C-u>", "<C-u>zz", { desc = "Page Up" })
-
 -- intend with tab
 map("n", "<tab>", ">>")
 map("n", "<S-tab>", "<<")
 map("v", "<tab>", ">gv")
 map("v", "<S-tab>", "<gv")
-
-map({"n", "v"}, "<leader>F", "<cmd>lua vim.lsp.buf.format({async=false})<cr>")
+-- format
+map({ "n", "v" }, "<leader>F", "<cmd>lua vim.lsp.buf.format({async=false})<cr>", { desc = "Format Buffer" })
