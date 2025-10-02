f5af85404455ded6b24f2815400d0603e50d8b73
lals-m3 <16458080+lalondesteve@users.noreply.github.com>
2025-10-02 19:03:49 -0400

    Changed Lazy to be default lazy to help when updating happens

--- lazy/lua/config/keymaps.lua
+++ lazy/lua/config/keymaps.lua
@@ -1,30 +1,3 @@
-local map = vim.keymap.set
-
--- map("n", "<leader>e", vim.cmd.Ex, { desc = "File Explorer" })
-map({ "n", "v" }, "<leader>te", "<cmd>:term<cr>i", { desc = "Open Terminal" })
-
--- lazy
-map("n", "<leader>ll", "<cmd>Lazy<cr>", { desc = "Lazy" })
-map("n", "<leader>lx", "<cmd>LazyExtras<cr>", { desc = "LazyExtras" })
-
--- intend with tab
-map("n", "<tab>", ">>")
-map("n", "<s-tab>", "<<")
-map("v", "<tab>", ">gv")
-map("v", "<s-tab>", "<gv")
-
--- move and zoom
-map("n", "<c-d>", "<c-d>zz", { desc = "Page Down" })
-map("n", "<c-u>", "<c-u>zz", { desc = "Page Up" })
--- move lines
-map({ "n" }, "<C-j>", ":m +1<cr>", { desc = "Move Line Down" })
-map({ "n" }, "<c-k>", ":m -2<cr>", { desc = "Move Line UP" })
-map({ "x" }, "<C-j>", ":m +1<cr>gv", { desc = "Move Selection Down" })
-map({ "x" }, "<c-k>", ":m -2<cr>gv", { desc = "Move Selection UP" })
-
--- Buffers ( : to leader)
-map({ "n", "v" }, "<leader>bn", vim.cmd.bnext, { desc = "Go to Next Buffer" })
-map({ "n", "v" }, "<leader>bp", vim.cmd.bprev, { desc = "Go to Previous Buffer" })
-map({ "n", "v" }, "<leader>bls", vim.cmd.buffers, { desc = "List All Buffers" })
-
-map({ "n", "v" }, "<leader>te", "<cmd>:term<cr>i", { desc = "Open Terminal" })
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
--- lazy/lua/config/options.lua
+++ lazy/lua/config/options.lua
@@ -1,6 +1,3 @@
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
-
-vim.g.lazyvim_eslint_auto_format = true
-vim.g.autoformat = true
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
