e1b3e834d8c450f2fd6ad2668312717174e3d2aa
lals-m3 <16458080+lalondesteve@users.noreply.github.com>
2025-11-11 12:10:54 -0500

    lazyvim update

--- nvim/lua/plugins/lazy-disable.lua
+++ nvim/lua/plugins/lazy-disable.lua
@@ -1,26 +1,31 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  -- {
-  --   "folke/snacks.nvim",
-  --   opts = {
-  --     dashboard = { enabled = false },
-  --     explorer = { enabled = false },
-  --     -- picker = { enabled = true },
-  --     -- indent = { enabled = true },
-  --     -- input = { enabled = true },
-  --     notifier = { enabled = false },
-  --     -- scope = { enabled = true },
-  --     -- scroll = { enabled = true },
-  --     -- statuscolumn = { enabled = true },
-  --     -- words = { enabled = true },
-  --     -- bigfile = { enabled = true },
-  --     -- quickfile = { enabled = true },
-  --   },
-  --   keys = {
-  --     { "<leader>e", false },
-  --     { "<leader>E", false },
-  --     { "<leader>fe", false },
-  --     { "<leader>FE", false },
-  --   },
-  -- },
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      dashboard = { enabled = false },
+      explorer = { enabled = false },
+      notifier = { enabled = false },
+
+      -- Default values --
+      -- picker = { enabled = true },
+      -- indent = { enabled = true },
+      -- input = { enabled = true },
+      -- scope = { enabled = true },
+      -- scroll = { enabled = true },
+      -- statuscolumn = { enabled = true },
+      -- words = { enabled = true },
+      -- bigfile = { enabled = true },
+      -- quickfile = { enabled = true },
+    },
+    keys = {
+      { "<leader>e", false },
+      { "<leader>E", false },
+      { "<leader>fe", false },
+      { "<leader>FE", false },
+    },
+  },
 }
