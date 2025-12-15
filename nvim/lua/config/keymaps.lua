ff028b8155a29bf17fe387f73d076282b00fc0e9
lals-m3 <16458080+lalondesteve@users.noreply.github.com>
2025-12-15 11:16:25 -0500

    small qol improvements

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -19,13 +19,13 @@ map("v", "<tab>", ">gv")
 map("v", "<s-tab>", "<gv")
 
 -- move and zoom
-map("n", "<c-d>", "<c-d>zz", { desc = "Page Down" })
+map("n", "<c-m>", "<c-m>zz", { desc = "Page Down" })
 map("n", "<c-u>", "<c-u>zz", { desc = "Page Up" })
 -- move lines
-map({ "n" }, "<C-j>", ":m +1<cr>", { desc = "Move Line Down" })
+map({ "n" }, "<c-j>", ":m +1<cr>", { desc = "Move Line Down" })
 map({ "n" }, "<c-k>", ":m -2<cr>", { desc = "Move Line UP" })
-map({ "x" }, "<C-j>", ":m +1<cr>gv", { desc = "Move Selection Down" })
-map({ "x" }, "<c-k>", ":m -2<cr>gv", { desc = "Move Selection UP" })
+map({ "v" }, "<c-j>", ":m +1<cr>gv", { desc = "Move Selection Down" })
+map({ "v" }, "<c-k>", ":m -2<cr>gv", { desc = "Move Selection UP" })
 
 -- Buffers ( : to leader)
 map({ "n", "v" }, "<leader>bn", vim.cmd.bnext, { desc = "Go to Next Buffer" })
